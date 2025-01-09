4e6097421213d301bb5acbcd180a8dd6f70e208d
vanha777 <vanha777@icloud.com>
2025-01-09 11:58:34 +1100

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ['tzqzzuafkobkhygtccse.supabase.co']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'biz-touch-7unj.shuttle.app',
+        pathname: '/api/**',
+      },
+    ],
   }
 };
 
